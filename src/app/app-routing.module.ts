4abf462bdb6610c4f03f714a91a5e2a38b2e85b3
kadir kaya <kadirkaya.13@outlook.com.tr>
2021-03-26 22:15:29 +0300

    26 mart

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CategoryComponent } from './components/category/category.component';
+import { LoginGuard } from './components/guards/login.guard';
+import { LoginComponent } from './components/login/login.component';
 import { ProductAddComponent } from './components/product-add/product-add.component';
 import { ProductComponent } from './components/product/product.component';
 
@@ -8,7 +10,9 @@ const routes: Routes = [
   {path:"",pathMatch:"full", component:ProductComponent},
   {path:"products",component:ProductComponent},
   {path:"products/category/:categoryId",component:ProductComponent},
-  {path:"products/add",component:ProductAddComponent}
+  {path:"products/add",component:ProductAddComponent,canActivate:[LoginGuard]},
+  {path:"login",component:LoginComponent}
+
 ];
 
 @NgModule({
